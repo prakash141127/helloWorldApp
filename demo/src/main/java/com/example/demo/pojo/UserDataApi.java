75c305148ba43d0c1fdf67195c2ccc7632052c79
durgamprakash <durgamprakash@172.20.10.10>
2025-03-19 14:16:49 +0530

    First Commit

---
+++ demo/src/main/java/com/example/demo/pojo/UserDataApi.java
@@ -0,0 +1,15 @@
+package com.example.demo.pojo;
+
+import lombok.Data;
+
+@Data
+public class UserDataApi {
+	
+	private String name;
+	private String email;
+	private String password;
+
+	
+	
+	
+}
